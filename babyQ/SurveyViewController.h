ef71f68afb76576a85eb2f8463bed428730d77f7
Chris Wood <chriswood@Chriss-MacBook-Pro.local>
2014-06-18 13:00:04 -0700

    started work on storing answer IDs

--- babyQ/SurveyViewController.h
+++ babyQ/SurveyViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SideSwipeTableViewController.h"
 
-@interface SurveyViewController : UIViewController
+@interface SurveyViewController : UIViewController <UIAlertViewDelegate>
 
 @property (nonatomic, retain) IBOutlet UIScrollView* scrollView;
 @property (nonatomic, retain) IBOutlet UIProgressView* progressView;
@@ -17,11 +18,14 @@
 @property (nonatomic, retain) IBOutlet UILabel* answerNumber;
 @property (nonatomic, retain) IBOutlet UITextView* question;
 @property (nonatomic, retain) IBOutlet UITextView* answerOne;
+@property (nonatomic, retain) IBOutlet UIButton* checkBoxOne;
 @property (nonatomic, retain) IBOutlet UIButton* nextButton;
 @property (nonatomic, retain) IBOutlet UIImageView* bottomDivider;
 
 @property (nonatomic,retain) NSDictionary* survey_json;
 @property (nonatomic,retain) NSString* question_number;
+@property (nonatomic, retain) NSMutableArray* answer_ids;
+@property (nonatomic, retain) NSMutableDictionary* selected_answers;
 
 - (IBAction)nextQuestion;
 
--- babyQ/SurveyViewController.m
+++ babyQ/SurveyViewController.m
@@ -14,7 +14,7 @@
 
 @implementation SurveyViewController
 
-@synthesize scrollView,progressView,progressBubble,questionNumber,answerNumber,question,answerOne,nextButton,bottomDivider,question_number,survey_json;
+@synthesize scrollView,progressView,progressBubble,questionNumber,answerNumber,question,answerOne,checkBoxOne,nextButton,bottomDivider,question_number,survey_json,answer_ids,selected_answers;
 
 - (void)viewDidLoad
 {
@@ -22,7 +22,7 @@
     [self.scrollView setBackgroundColor:[UIColor whiteColor]];
     
     CGAffineTransform transform = CGAffineTransformMakeScale(1.0f, 3.0f);
-    self.progressView.transform = transform;
+    progressView.transform = transform;
     
     self.navigationController.navigationBarHidden = NO;
     self.navigationController.navigationBar.backItem.title = @"";
@@ -118,29 +118,44 @@
         
     }
     
+    float progress = ([question_number floatValue] - 1) / [survey_json[@"ScoringQuestions"] count];
+    progressView.progress = progress;
+    [progressBubble setFrame:CGRectMake(progressBubble.frame.origin.x + (295-26)*progress, progressBubble.frame.origin.y, progressBubble.frame.size.width, progressBubble.frame.size.height)];
+    
     question.text = survey_json[@"ScoringQuestions"][question_number][@"Question"];
     
     answerOne.text = survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"][@"1"][@"Answer"];
-    NSUInteger numberOfQuestions = [survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"] count];
+    checkBoxOne.tag = [question_number intValue]-1;
+    [checkBoxOne addTarget:self action:@selector(clickedAnswer:) forControlEvents:UIControlEventTouchUpInside];
+
+    [answer_ids addObject:survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"][@"1"][@"PossibleAnswerId"]];
+    NSUInteger numberOfAnswers = [survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"] count];
     for (int i = 2; i <= [survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"] count]; i++)
     {
         UITextView* nextAnswer = [[UITextView alloc] initWithFrame:CGRectMake(45, 324 + 65*(i-1), 189, 54)];
         nextAnswer.backgroundColor = [UIColor clearColor];
         nextAnswer.editable = NO;
         nextAnswer.userInteractionEnabled = NO;
-        nextAnswer.text = survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"][[NSString stringWithFormat:@"%i", i]][@"Answer"];
+        NSString* i_string = [NSString stringWithFormat:@"%i", i];
+        nextAnswer.text = survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"][i_string][@"Answer"];
         [self.scrollView addSubview:nextAnswer];
         
         UIButton* checkBox = [UIButton buttonWithType:UIButtonTypeCustom];
+        checkBox.tag = [question_number intValue]-1;
         [checkBox setFrame:CGRectMake(265, 332+65*(i-1), 16, 16)];
         [checkBox setBackgroundImage:[UIImage imageNamed:@"babyq_circle.png"] forState:UIControlStateNormal];
+        [checkBox addTarget:self action:@selector(clickedAnswer:) forControlEvents:UIControlEventTouchUpInside];
         [self.scrollView addSubview:checkBox];
+        
+        
+        
+        [answer_ids addObject:survey_json[@"ScoringQuestions"][question_number][@"PossibleAnswers"][i_string][@"PossibleAnswerId"]];
     }
-    if (numberOfQuestions > 1)
+    if (numberOfAnswers > 1)
     {
-        [self.scrollView setContentSize:CGSizeMake(320, 500 + 65 * (numberOfQuestions-1) )];
-        [self.bottomDivider setFrame:CGRectMake(bottomDivider.frame.origin.x, bottomDivider.frame.origin.y+65*(numberOfQuestions-1), bottomDivider.frame.size.width, bottomDivider.frame.size.height)];
-        [self.nextButton setFrame:CGRectMake(nextButton.frame.origin.x, nextButton.frame.origin.y +65*(numberOfQuestions-1), nextButton.frame.size.width, nextButton.frame.size.height)];
+        [self.scrollView setContentSize:CGSizeMake(320, 500 + 65 * (numberOfAnswers-1) )];
+        [self.bottomDivider setFrame:CGRectMake(bottomDivider.frame.origin.x, bottomDivider.frame.origin.y+65*(numberOfAnswers-1), bottomDivider.frame.size.width, bottomDivider.frame.size.height)];
+        [self.nextButton setFrame:CGRectMake(nextButton.frame.origin.x, nextButton.frame.origin.y +65*(numberOfAnswers-1), nextButton.frame.size.width, nextButton.frame.size.height)];
     }
 }
 
@@ -149,15 +164,49 @@
     self.navigationController.navigationBar.topItem.title = @"Take Survey";
 }
 
+- (void) clickedAnswer:(UIButton*)sender
+{
+    [selected_answers setObject:answer_ids[sender.tag] forKey:question_number];
+    [sender setBackgroundImage:[UIImage imageNamed:@"babyq_circle_orange.png"] forState:UIControlStateNormal];
+}
+
 - (IBAction)nextQuestion
 {
     SurveyViewController* surveyController = [self.storyboard instantiateViewControllerWithIdentifier:@"MultipleChoice"];
     NSInteger question_int = [question_number integerValue];
-    if (question_int < 21)
+    NSUInteger numberOfQuestions = [survey_json[@"ScoringQuestions"] count];
+    if (question_int < numberOfQuestions)
+    {
         surveyController.question_number = [NSString stringWithFormat:@"%li",question_int + 1];
-    surveyController.survey_json = self.survey_json;
-    [self.navigationController pushViewController:surveyController animated:YES];
-    self.navigationController.navigationBarHidden = YES;
+        surveyController.survey_json = self.survey_json;
+        [self.navigationController pushViewController:surveyController animated:YES];
+        self.navigationController.navigationBarHidden = YES;
+    }
+    else
+    {
+        NSString* title = @"Survey Complete";
+        NSString* message = @"Thanks for taking the survey! We've calculated your new babyQ score, go check it out!";
+        NSString* buttonTitle = @"VIEW BABYQ SCORE";
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:self cancelButtonTitle:buttonTitle otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    UIStoryboard * homeScreens = [UIStoryboard storyboardWithName:@"HomePage" bundle:nil];
+    SideSwipeTableViewController* sideSwipeTableView = [homeScreens instantiateViewControllerWithIdentifier:@"SideSwipeTableView"];
+    
+    CurrentScoreViewController* currentScoreView = [homeScreens instantiateViewControllerWithIdentifier:@"CurrentScoreView"];
+    
+    MMDrawerController * swipeController = [[MMDrawerController alloc]
+                                            initWithCenterViewController:currentScoreView
+                                            leftDrawerViewController:sideSwipeTableView
+                                            rightDrawerViewController:nil];
+    [swipeController setOpenDrawerGestureModeMask:MMOpenDrawerGestureModePanningCenterView];
+    [swipeController setCloseDrawerGestureModeMask:MMCloseDrawerGestureModeBezelPanningCenterView];
+    
+    [self.navigationController pushViewController:swipeController animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
